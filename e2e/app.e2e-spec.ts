80f9026ae7e8884ee11f05360ff90156aa89062b
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-06-09 16:01:02 +0200

    New version

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,16 @@
 import { DrawAndGuessGamePage } from './app.po';
 
-describe('draw-and-guess-game App', function() {
+describe('draw-and-guess-game App', () => {
   let page: DrawAndGuessGamePage;
 
   beforeEach(() => {
     page = new DrawAndGuessGamePage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', done => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
   });
 });
