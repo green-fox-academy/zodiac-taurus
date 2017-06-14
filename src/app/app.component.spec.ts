2eec9bb908a21aae5b0b0e6a1b4b622e393d350d
Szilardoo <doczi.szilard@gmail.com>
2017-06-14 10:31:41 +0200

    fix(test): unnecesarry test deleted

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,11 +26,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Draw and Guess game');
   }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Draw and Guess game');
-  }));
 });
