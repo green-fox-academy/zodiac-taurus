81bf07547cef7d5ee88d37797fec39fd573444ba
Gabor V <gabor.w.varga@gmail.com>
2017-06-19 21:54:15 +0200

    feat(login): first room on display with *ngFor parses GET request's response

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,11 +11,12 @@ import { HttpService } from '../http.service';
 export class HomeComponent implements OnInit {
 
   error: string;
+  rooms = [];
 
   constructor(private httpService: HttpService) {
 
     this.httpService.listAllRoom().subscribe(
-      (response) => console.log(response),
+      (response) => this.listRoom(response),
       // (error) => this.error = 'Authentication error'
       (error) => console.log(error)
     );
@@ -24,8 +25,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() { }
 
-  createRoom() {
-    console.log('create room');
+  listRoom(response) {
+    this.rooms = response.json();
+    console.log(this.rooms);
+    return this.rooms;
   }
 
 }
